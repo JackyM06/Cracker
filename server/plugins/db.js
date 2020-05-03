bf6833b5053338b60fafd009c49016bb188cdb66
JackyMao <ChinaMJK06@163.com>
2020-05-03 20:47:02 +0800

    细节修复

--- server/plugins/db.js
+++ server/plugins/db.js
@@ -11,5 +11,5 @@ module.exports = (app)=>{
         }
     })
     // app.use(mongoose)
-    require('require-all')(__dirname+'../../models')
+    // require('require-all')(__dirname+'../../models')
 }
